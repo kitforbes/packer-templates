785b3b09e660876caac06d2680d729ead78a8a80
Chris Forbes <kitforbes@users.noreply.github.com>
2018-07-30 20:47:35 +0100

    Fix syntax errors

--- scripts/verify.ps1
+++ scripts/verify.ps1
@@ -6,12 +6,12 @@ $ProgressPreference = "SilentlyContinue"
 Write-Output "==> Default Packer environment variables..."
 Write-Output "PACKER_BUILD_NAME   : $(Get-PackerBuildName)"
 Write-Output "PACKER_BUILDER_TYPE : $(Get-PackerBuildType)"
-Write-Output "PACKER_HTTP_ADDR    : $(Get-PackerHttpAddr)"
+Write-Output "PACKER_HTTP_ADDR    : $(Get-PackerHttpAddress)"
 
 Write-Output "", "==> PowerShell Version information..."
 $PSVersionTable.PSVersion
 
-if ((Get-PowerShellVersion).Split(".")[0..1].Join(".") -ne "5.1") {
+if (((Get-PowerShellVersion).Split(".")[0..1] -Join ".") -ne "5.1") {
     Write-Output "Failing due to PowerShell 5.1 not being present, found v$(Get-PowerShellVersion)"
     exit 1
 }
