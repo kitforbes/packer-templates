d18de51d96c8507c027d491c6bfd9b38c9037ec2
Chris Forbes <kitforbes@users.noreply.github.com>
2018-08-19 17:14:39 +0100

    Set rules for remote desktop access

--- cookbooks/provision/recipes/powershell_modules.rb
+++ cookbooks/provision/recipes/powershell_modules.rb
@@ -4,3 +4,18 @@ powershell_package 'PSWindowsUpdate' do
   version '2.0.0.4'
   action :install
 end
+
+powershell_package 'xNetworking' do
+  version '5.7.0.0'
+  action :install
+end
+
+powershell_package 'xRemoteDesktopAdmin' do
+  version '1.1.0.0'
+  action :install
+end
+
+powershell_package 'xCertificate' do
+  version '3.2.0.0'
+  action :install
+end
